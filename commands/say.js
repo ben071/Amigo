202e6ea063ae9872e8f32f2ac6a563fa9ab7b6d7
ben071 <benjaminhiggs@protonmail.com>
2018-10-15 23:16:30 +0100

    ✨ Add say command

---
+++ commands/say.js
@@ -0,0 +1,19 @@
+exports.run = async (client, message, args) => {
+  if (!args[0]) return message.reply("There was nothing to say");
+  const text = args.join(" ");
+  message.channel.send(text);
+  message.delete();
+};
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["echo","say"],
+  permLevel: "Moderator"
+};
+ 
+exports.help = {
+  name: "repeat",
+  category: "Moderation",
+  description: "Repeats the message you say, useful for information channels or announcements",
+  usage: "repeat [text]"
+};
