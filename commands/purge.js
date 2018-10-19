c1375c9c4ed6144a5cda5c53365b1d8c9c603e5a
ben071 <benjaminhiggs@protonmail.com>
2018-10-19 20:37:09 +0100

    ✨ Add purge logs

--- commands/purge.js
+++ commands/purge.js
@@ -1,13 +1,31 @@
+const Discord = require("discord.js");
+const { inspect } = require("util");
+
 exports.run = (client, message, args) => {
+  const settings = message.settings;
   const messagecount = parseInt(args.join(" "));
+  inspect((settings), {code: "json"});
+  const modLog = message.guild.channels.find(c => c.name === settings.modLogChannel);
+  if (!modLog) return message.reply("I cannot find a mod-log channel");
+  if (isNaN(messagecount)) return message.channel.send("Enter a valid integer.");
+  if (messagecount > 100) {
+    message.channel.send("Please enter a number less than or equal to 100.");
+    return;
+  };
   message.channel.fetchMessages({
     limit: messagecount
-  }).then(messages => message.channel.bulkDelete(messages));
+  }).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+
+  const embed = new Discord.RichEmbed()
+    .setColor("#FF4848")
+    .setTimestamp()
+    .setDescription(`**Action:** Purge\n**Moderator:** ${message.author.tag}\n**Number of messages:** ${messagecount}\n**Channel:** ${message.channel.name}`);
+  return client.channels.get(modLog.id).send({embed});
 };
 
 exports.conf = {
   enabled: true,
-  guildOnly: false,
+  guildOnly: true,
   aliases: [],
   permLevel: "Moderator"
 };
