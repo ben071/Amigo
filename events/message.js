d9c1ed8277ccd159c747059d27856a81010bd4b4
Jack <40802798+Jack073@users.noreply.github.com>
2018-11-20 18:06:00 +0000

    :sparkles: add message and command counter to message event

--- events/message.js
+++ events/message.js
@@ -1,13 +1,56 @@
+const messageData = require("../messageData.json");
+const fs = require("fs");
+const moment = require("moment");
+require("moment-duration-format");
 module.exports = (client, message) => {
+
+  if (!messageData[message.guild.id]) {
+      messageData[message.guild.id] = {
+          messages: 0,
+          commandsRan: 0
+      }
+  };
+  if (!messageData["totalMessages"]) {
+      messageData["totalMessages"] = {
+          messages: 0,
+          commandsRan: 0
+      }
+  }
+  if (!messageData[message.guild.id].lastUpdate) {
+      messageData[message.guild.id] = {
+          messages: messageData[message.guild.id].messages,
+          commandsRan: messageData[message.guild.id].commandsRan,
+          lastUpdate: moment().format("MMMM Do YYYY")
+      }
+  }
+    
+  if (messageData[message.guild.id].lastUpdate != moment().format("MMMM Do YYYY")) { //Check that the last reset wasn't today, then reset
+      messageData[message.guild.id] = {
+          messages: 0,
+          commandsRan: 0,
+          lastUpdate: moment().format("MMMM Do YYYY")
+      }
+      messageData["totalMessages"] = {
+          messages: 0,
+          commandsRan: 0
+      }
+  }
+  messageData[message.guild.id].messages++
+  messageData["totalMessages"].messages++
+  fs.writeFile("./messageData.json", JSON.stringify(messageData), (err) => { //Save data file
+    if (err) console.log(err);
+  });
   if (message.author.bot) return;
   const settings = message.settings = client.getGuildSettings(message.guild);
   if (message.content.indexOf(settings.prefix) !== 0) return;
   if (!message.channel.permissionsFor(client.user).has("SEND_MESSAGES")) return;
 
+
   const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   const level = client.permlevel(message);
   const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command));
+
   if (!cmd) return;
 
   if (cmd && !message.guild && cmd.conf.guildOnly)
@@ -31,5 +74,10 @@ module.exports = (client, message) => {
   }
 
   client.logger.cmd(`[CMD] ${client.config.permLevels.find(l => l.level === level).name} ${message.author.username} (${message.author.id}) ran command ${cmd.help.name}`);
+  messageData[message.guild.id].commandsRan++
+  messageData["totalMessages"].commandsRan++
+  fs.writeFile("./messageData.json", JSON.stringify(messageData), (err) => { //Save data file again incase any commands were run
+    if (err) console.log(err);
+  });
   cmd.run(client, message, args, level);
 };
