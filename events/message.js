f446dc22da25af9eada550f2f273c0f76b18efd6
Jack <40802798+Jack073@users.noreply.github.com>
2019-01-02 19:51:36 +0000

    add total message counter to message event

--- events/message.js
+++ events/message.js
@@ -3,8 +3,9 @@ const messageData = require("../messageData.json");
 const fs = require("fs");
 const moment = require("moment");
 require("moment-duration-format");
-
 module.exports = (client, message) => {
+  if (message.author.bot) return;
+  if (!message.guild) return message.reply("Commands are not available in DM");
   if (message.guild) {
 
     if (!messageData[message.guild.id]) {
@@ -40,44 +41,44 @@ module.exports = (client, message) => {
           messages: 0,
           commandsRan: 0
       }
-
-      messageData["totalMessages"] = {
-          messages: 0,
-          commandsRan: 0
-      }
   }
+  if (!messageData["unreset"]) {
+    messageData["unreset"] = {
+      messages: 0,
+      commandsRan: 0
+    }
+  }
+  messageData["unreset"].messages++
   messageData["totalMessages"].messages++
   fs.writeFile("./messageData.json", JSON.stringify(messageData), (err) => { //Save data file
     if (err) console.log(err);
   });
-  if (message.author.bot) return;
-  if (message.guild === null) return message.reply("Commands aren't available in DM.");
   const settings = message.settings = client.getGuildSettings(message.guild);
   if (message.content.indexOf(settings.prefix) !== 0) return;
   if (message.guild) {
     if (!message.channel.permissionsFor(client.user).has("SEND_MESSAGES")) return;
   }
-
   const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   const level = client.permlevel(message);
   const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command));
 
   if (!cmd) return;
+  
+  if (cmd && message.guild.id != '508320098562605066' && cmd.conf.devGuildOnly) return message.channel.send("This command can only be used in the Amigo Support guild. Please check back to see when it's available for general use.")
 
-  if (cmd && message.guild.id != '508320098562605066' && cmd.conf.devGuildOnly)
-    return message.channel.send("This command can only be used in the Amigo Support guild. Please check back to see when it's available for general use.")
-
-  if (level < client.levelCache[cmd.conf.permLevel]) {
-    const embed = new Discord.RichEmbed()
-      .setTitle("⚠ Missing Permissions!")
-      .setTimestamp()
-      .setColor("#FF0000")
-      .addField("Permission Level:", `${level} (${client.config.permLevels.find(l => l.level === level).name})`)
-      .addField("Required Level: ", `${client.levelCache[cmd.conf.permLevel]} (${cmd.conf.permLevel})`)
-      .setFooter(message.author.tag, message.author.avatarURL);
-    return message.channel.send(embed);
-  }
+  if (cmd && !message.guild && cmd.conf.guildOnly) return message.channel.send("This command is unavailable via private message. Please run this command in a guild.");
+    if (level < client.levelCache[cmd.conf.permLevel]) {
+      const embed = new Discord.RichEmbed()
+        .setTitle("⚠ Missing Permissions!")
+        .setTimestamp()
+        .setColor("#FF0000")
+        .addField("Permission Level:", `${level} (${client.config.permLevels.find(l => l.level === level).name})`)
+        .addField("Required Level: ", `${client.levelCache[cmd.conf.permLevel]} (${cmd.conf.permLevel})`)
+        .setFooter(message.author.tag, message.author.avatarURL);
+      return message.channel.send(embed);
+    }
+  
 
   message.author.permLevel = level;
 
@@ -90,6 +91,7 @@ module.exports = (client, message) => {
   if (message.guild) {
     messageData[message.guild.id].commandsRan++
   }
+  messageData["unreset"].commandsRan++
   messageData["totalMessages"].commandsRan++
   fs.writeFile("./messageData.json", JSON.stringify(messageData), (err) => { //Save data file again incase any commands were run
     if (err) console.log(err);
