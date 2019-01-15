6e9c0a239f76b60354caa723ca0d4fd3cef801ba
ben071 <benjaminhiggs@icloud.com>
2019-01-15 22:03:15 +0000

    Update message.js

--- events/message.js
+++ events/message.js
@@ -3,55 +3,9 @@ const fs = require("fs");
 const moment = require("moment");
 require("moment-duration-format");
 module.exports = (client, message) => {
-  /*if (message.author.bot) return;
+  if (message.author.bot) return;
   if (!message.guild) return message.reply("Commands are not available in DM");
-  if (message.guild) {
 
-    if (!messageData[message.guild.id]) {
-        messageData[message.guild.id] = {
-            messages: 0,
-            commandsRan: 0
-        }
-  }
-  if (!messageData[message.guild.id].lastUpdate) {
-      messageData[message.guild.id] = {
-          messages: messageData[message.guild.id].messages,
-          commandsRan: messageData[message.guild.id].commandsRan,
-          lastUpdate: moment().format("MMMM Do YYYY")
-      }
-  }
-    
-  if (messageData[message.guild.id].lastUpdate != moment().format("MMMM Do YYYY")) { //Check that the last reset wasn't today, then reset
-      messageData[message.guild.id] = {
-          messages: 0,
-          commandsRan: 0,
-          lastUpdate: moment().format("MMMM Do YYYY")
-      }
-      messageData["totalMessages"] = {
-          messages: 0,
-          commandsRan: 0
-      }
-    }
-  messageData[message.guild.id].messages++
-
-};
-  if (!messageData["totalMessages"]) {
-      messageData["totalMessages"] = {
-          messages: 0,
-          commandsRan: 0
-      }
-  }
-  if (!messageData["unreset"]) {
-    messageData["unreset"] = {
-      messages: 0,
-      commandsRan: 0
-    }
-  }
-  messageData["unreset"].messages++
-  messageData["totalMessages"].messages++
-  fs.writeFile("./messageData.json", JSON.stringify(messageData), (err) => { //Save data file
-    if (err) console.log(err);
-  });*/
   const settings = message.settings = client.getGuildSettings(message.guild);
   if (message.content.indexOf(settings.prefix) !== 0) return;
   if (message.guild) {
@@ -87,13 +41,5 @@ module.exports = (client, message) => {
   }
 
   client.logger.cmd(`[CMD] ${client.config.permLevels.find(l => l.level === level).name} ${message.author.username} (${message.author.id}) ran command ${cmd.help.name} in ${message.guild.name}`);
-  if (message.guild) {
-    messageData[message.guild.id].commandsRan++
-  }
-  messageData["unreset"].commandsRan++
-  messageData["totalMessages"].commandsRan++
-  fs.writeFile("./messageData.json", JSON.stringify(messageData), (err) => { //Save data file again incase any commands were run
-    if (err) client.logger.error(err);
-  });
   cmd.run(client, message, args, level);
 };
