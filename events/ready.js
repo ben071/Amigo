983a8febc47b5d9d4a1f67f3ac9cd3ff171e6856
Jack <40802798+Jack073@users.noreply.github.com>
2019-01-17 22:32:56 +0000

    add reaction roles

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,10 @@
 const config = require("../config.js")
 const DBL = require("dblapi.js");
+const moment = require("moment");
+const reactionRoleSetup = require("../reaction roles.json")
 
 module.exports = async client => {
+  const timestamp = `${moment().format("YYYY-MM-DD HH:mm:ss")}`;
   const dbl = new DBL(config.dbltoken, client);
 
   setInterval(() => {
@@ -16,7 +19,20 @@ module.exports = async client => {
     console.log(`Oops! ${e}`);
   })
 
-  client.logger.log(`[READY] ${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, "ready");
+  client.logger.log(`\`\`\`Restart - ${timestamp}\`\`\`\n[READY] ${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, "ready");
   
   client.user.setActivity(`${client.config.defaultSettings.prefix}help`, {type: "PLAYING"});
+
+  for (const key in reactionRoleSetup) { // fetch all messages in the reaction role config to allow us to recieve reactions on them
+    const setup = reactionRoleSetup[key]
+    const server = client.guilds.find(g => g.id == setup.guildID)
+    if (!server) continue; // check it is a valid server
+    const channel = server.channels.find(c => c.id == setup.channelID)
+    if (!channel) continue; // check it is a valid channel
+    try{
+      channel.fetchMessage(setup.messageID) // fetch the message
+    } catch (err) {
+      continue
+    }
+  }
 };
