a051b13fbdef856d92ac93743ffd61f21b7ae561
ben071 <benjaminhiggs@icloud.com>
2019-02-14 23:31:27 +0000

    Remove the constant log spam

--- events/guildMemberRemove.js
+++
@@ -1,3 +0,0 @@
-module.exports = async (client, member) => {
-    client.logger.log(`<@${member.user.id}> has left ${member.guild.name}. This guild now has ${member.guild.memberCount} members.`);
-};
