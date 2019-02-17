18bdc52a8c5ba0e67552e1e6961e50f12899d552
ben071 <benjaminhiggs@icloud.com>
2019-02-17 16:38:29 +0000

    Fix some indents

--- index.js
+++ index.js
@@ -7,39 +7,37 @@ const databaseFile = require("./modules/db.js");
 client.db = new databaseFile();
 client.db.init();
 
-require("./utils/functions.js")(client);
-client.logger = require("./utils/logger.js")
+require("./modules/functions.js")(client);
+client.logger = require("./utils/logger.js");
 client.commands = new Discord.Collection();
 const config = require("./config.json");
 
 const init = async () => {
-    // I am actually gonna comment this code too
+  const cmdFiles = await readdir("./commands/");
+  client.logger.log(`Loading a total of ${cmdFiles.length} commands.`);
+  cmdFiles.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let cmdFunction = require(`./commands/${file}`);
+    client.commands.set(cmdFunction.help.name, cmdFunction);
+  });
 
-    const cmdFiles = await readdir("./commands/");
-    client.logger.log(`Loading a total of ${cmdFiles.length} commands.`);
-    cmdFiles.forEach(file => {
-        if (!file.endsWith(".js")) return;
-        let cmdFunction = require(`./commands/${file}`);
-        client.commands.set(cmdFunction.help.name, cmdFunction);
-    });
-
-    const evtFiles = await readdir("./events/");
-    client.logger.log(`Loading a total of ${evtFiles.length} events.`);
-    evtFiles.forEach(file => {
-      const eventName = file.split(".")[0];
-      const event = require(`./events/${file}`);
-      client.on(eventName, event.bind(null, client));
-      const mod = require.cache[require.resolve(`./events/${file}`)];
-      delete require.cache[require.resolve(`./events/${file}`)];
-      for (let i = 0; i < mod.parent.children.length; i++) {
-        if (mod.parent.children[i] === mod) {
-          mod.parent.children.splice(i, 1);
-          break;
-        }
+  const evtFiles = await readdir("./events/");
+  client.logger.log(`Loading a total of ${evtFiles.length} events.`);
+  evtFiles.forEach(file => {
+    const eventName = file.split(".")[0];
+    const event = require(`./events/${file}`);
+    client.on(eventName, event.bind(null, client));
+    const mod = require.cache[require.resolve(`./events/${file}`)];
+    delete require.cache[require.resolve(`./events/${file}`)];
+    for (let i = 0; i < mod.parent.children.length; i++) {
+      if (mod.parent.children[i] === mod) {
+        mod.parent.children.splice(i, 1);
+        break;
       }
-    });  
+    }
+  });
 
-    client.login(config.token);
+  client.login(config.token);
 
 };
 
