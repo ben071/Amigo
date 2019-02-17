b2eda51308a63b2ce1eca678c26f4174539513fe
ben071 <benjaminhiggs@icloud.com>
2019-02-17 16:35:48 +0000

    Update database to add mod logs

--- modules/db.js
+++ modules/db.js
@@ -1,37 +1,43 @@
 const rethink = require("rethinkdbdash");
 
-module.exports = class { 
+module.exports = class {
   constructor() {
-      this.r = rethink({
-          db: "Amigo",
-      })
+    this.r = rethink({
+      db: "Amigo",
+    })
   }
-  
+
   init() {
-    if(this.r.table("guilds")) return;
-    return this.r.tableCreate('guilds').run() 
-    .then(() => console.log("Guild and settings table created."))
-    .catch((e) => {
-        if (e.name === "ReqlOpFailedError") {
-        } else {
-            console.error(`There was an unexpected error with the database. ${e}. Exiting. Please ignore all the text spammed at the start of console`);
-            process.exit(1);
+    if (this.r.table("guilds")) return;
+    return this.r.tableCreate('guilds').run()
+      .then(() => console.log("Guild and settings table created."))
+      .catch((e) => {
+        if (e.name === "ReqlOpFailedError") {} else {
+          console.error(`There was an unexpected error with the database. ${e}. Exiting. Please ignore all the text spammed at the start of console`);
+          process.exit(1);
         }
-    });
-  } 
-  
+      });
+  }
+
   createGuild(guild) {
     return this.r.table('guilds').insert([{
         id: guild.id,
         guildname: guild.name,
-        prefix: "a]",
-    }]).run()
-    .catch((e) => console.log(e))
+        prefix: "a!!",
+        modLogChannel: "amigo-logs"
+      }]).run()
+      .catch((e) => console.log(e))
   }
 
   updatePrefix(guildID, newPrefix) {
     return this.r.table('guilds').get(guildID).update({
-        prefix: newPrefix
+      prefix: newPrefix
+    }).run();
+  }
+
+  updateLogs(guildID, newLogs) {
+    return this.r.table('guilds').get(guildID).update({
+      modLogChannel: newLogs
     }).run();
   }
 }
