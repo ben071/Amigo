5c3c122cf3483cfc352111a63300d357fd2325c2
ben071 <benjaminhiggs@icloud.com>
2019-02-17 16:36:20 +0000

    Add ready event

---
+++ events/ready.js
@@ -0,0 +1,10 @@
+const moment = require("moment");
+
+module.exports = async client => {
+    const timestamp = `${moment().format("YYYY-MM-DD HH:mm:ss")}`;
+    client.logger.log(`\`\`\`${timestamp}\`\`\`\n[READY] ${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, "ready");
+
+    client.user.setActivity(`for a!!help | amigo.fun | Amigo Rewrite`, {
+        type: "WATCHING"
+    });
+}
