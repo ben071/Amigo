f07bfc5447065908461e1dfc47149198abe3b28d
ben071 <benjaminhiggs@icloud.com>
2019-02-17 16:36:52 +0000

    Add check for sending messages

--- events/message.js
+++ events/message.js
@@ -1,14 +1,16 @@
-const Discord = require("discord.js");
-
 module.exports = async (client, message) => {
     if (message.author.bot) return;
     if (!message.guild) return message.reply("Commands are not available in DM");
+
+    if (message.guild) {
+        if (!message.channel.permissionsFor(client.user).has("SEND_MESSAGES")) return;
+    }
 
     let prefix = await client.db.r.table("guilds").get(message.guild.id).getField("prefix").run()
 
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
-    
+
     const cmd = client.commands.get(command)
     if (!cmd) return;
 
