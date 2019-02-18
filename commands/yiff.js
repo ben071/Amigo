fe8266d7b678f9ee45f5e97560d682a2e03f502c
ben071 <benjaminhiggs@icloud.com>
2019-02-18 18:30:53 +0000

    Add yiff commands (work in progress)

---
+++ commands/yiff.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+const config = require("../config.json");
+const yiff = require("yiff");
+
+exports.run = async (client, message, args) => {
+    if (await client.helpArgs(client, message, args, exports)) return;
+    if (!args[0]) return errors.noArgs(message, exports);
+    const prop = args.slice(1, 4).join("+");
+    if(args[0] === "e621") {
+        try {
+            await yiff.e621.CubFilter(prop).then(r => {
+                const embed = new Discord.RichEmbed()
+                    .setColor(config.green)
+                    .setAuthor("e621")
+                    .setDescription(`${r.tags}`)
+                    .setImage(r.image)
+                    .setFooter(`Artist: ${r.artist.join(" ")} | Score: ${r.score} | Fav. Count: ${r.fav_count} | ID: ${r.postID}`);
+                message.channel.send(embed)
+            });
+        } catch (e) {
+            return errors.tagNotFound(message, args);
+        }
+    };
+};
+
+exports.help = {
+    name: "yiff",
+    description: "Gets a random yiff image.",
+    usage: "yiff [website] <tags>"
+};
+
+exports.conf = {
+    permission: "SEND_MESSAGES",
+    NSFWCommand: true,
+};
