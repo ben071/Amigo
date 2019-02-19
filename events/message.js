71ab91b750094f835f48202c09b53237bdb118f4
ben071 <benjaminhiggs@icloud.com>
2019-02-19 22:53:47 +0000

    Remove database recreation on each message

--- events/message.js
+++ events/message.js
@@ -1,13 +1,12 @@
 const errors = require("../utils/errors.js")
 
 module.exports = async (client, message) => {
-    await client.db.createGuild(message.guild);
     if (message.author.bot) return;
-    if (!message.guild) return message.reply("Commands are not available in DM");
+    if (!message.guild) return message.channel.send("Commands are not available in DM");
 
     if (message.guild) {
         if (!message.channel.permissionsFor(client.user).has("SEND_MESSAGES")) return;
-    }
+    };
 
     let prefix = await client.db.r.table("guilds").get(message.guild.id).getField("prefix").run()
     if(message.content.indexOf(prefix) !== 0) return;
