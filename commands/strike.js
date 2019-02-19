bf99f7130c7ca5b80d810a87e8b2db0b6439458c
Ben <benjaminhiggs@protonmail.com>
2019-02-19 17:01:49 +0000

    Add strike command

---
+++ commands/strike.js
@@ -0,0 +1,31 @@
+const errors = require("../utils/errors.js");
+
+exports.run = async (client, message, args) => {
+    if (await client.helpArgs(client, message, args, exports)) return;
+    if (!message.member.hasPermission(exports.conf.permission)) return errors.noPermissions(message, exports);
+    if (!args[0]) return errors.noArgs(message, exports);
+
+    const modLogs = await client.db.r.table("guilds").get(message.guild.id).getField("modLogChannel").run();
+    if (!await client.findLogs(client, message, modLogs)) return;
+    const type = exports.help.name.toProperCase();
+
+    let user = message.guild.member(message.mentions.members.first());
+    if (!user) return errors.invalidUser(message, args);
+
+    let reason = args.slice(1).join(" ");
+    if (!reason) return errors.invalidReason(message);
+
+    //if (user.hasPermission(exports.conf.permission)) return errors.cannotPunish(message);
+    await client.db.createPunish(client, message, type, user, reason, modLogs);
+    client.logger.log(`${message.author.username} has stricken ${user.user.username} from ${message.guild} for ${reason}.`);
+};
+
+exports.help = {
+    name: "strike",
+    description: "Strikes a user.",
+    usage: "strike [@user] <reason>"
+};
+
+exports.conf = {
+    permission: "MANAGE_MESSAGES",
+};
