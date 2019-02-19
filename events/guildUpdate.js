6d97086dd3fb803e597d9c09389ef59c1935be8b
Ben <benjaminhiggs@protonmail.com>
2019-02-19 17:04:24 +0000

    Update guild database on add/change

---
+++ events/guildUpdate.js
@@ -0,0 +1,6 @@
+module.exports = async (client, oldGuild, newGuild) => {
+    if(oldGuild.name !== newGuild.name) {
+        client.db.updateGuildName(newGuild.id, newGuild.name);
+        client.logger.log(`Guild **${oldGuild.name}** - (ID: ${newGuild.id}) has changed it's name to **${newGuild.name}**`);
+    };
+}
