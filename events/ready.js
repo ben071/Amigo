409d684f7ed77c8b9b9f309d85aa2371bb3ed953
Ben <benjaminhiggs@protonmail.com>
2019-02-21 16:50:28 +0000

    Change activity for main bot

--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,7 @@ module.exports = async client => {
     const timestamp = `${moment().format("YYYY-MM-DD HH:mm:ss")}`;
     client.logger.log(`\`\`\`${timestamp}\`\`\`\n[READY] ${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, "ready");
 
-    client.user.setActivity(`for a!!help | amigo.fun | Amigo Rewrite`, {
+    client.user.setActivity(`for a!help | amigo.fun | Rewriting...`, {
         type: "WATCHING"
     });
 }
