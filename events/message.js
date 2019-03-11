12748ec05bfd9f0c5c6268190c66c3228183fcee
Jack <40802798+Jack073@users.noreply.github.com>
2019-03-11 00:33:06 +0000

    Add DM bot owner if someone DM's the bot

--- events/message.js
+++ events/message.js
@@ -1,12 +1,34 @@
 const errors = require("../utils/errors.js");
-
+const config = require("../config.json");
+const Discord = require("discord.js")
 module.exports = async (client, message) => {
     if (message.author.bot) return;
-    if (!message.guild) return message.channel.send("Commands are not available in DM");
-
-    if (message.guild) {
-        if (!message.channel.permissionsFor(client.user).has("SEND_MESSAGES")) return;
-    }
+    if (!message.guild) { //return message.channel.send("Commands are not available in DM");
+        if (message.content.startsWith(config.defaultPrefix)) {
+            return;
+        } else {
+            const dmEmbed = new Discord.RichEmbed()
+            .setTitle("DM Recieved")
+            .setDescription(`DM from ${message.author.tag} (${message.author.id})\n**Content**:\n${message.content}`)
+            .setThumbnail(message.author.avatarURL || message.author.defaultAvatarURL)
+            .addField(`Mutual Servers with ${client.user.username}`, client.guilds.filter(g => g.members.find(m => m.id == message.author.id)).size)
+            .addField("Account created on", message.author.createdAt.toUTCString())
+            .setColor(config.red)
+            .setTimestamp();
+            const owner = await client.fetchUser(config.ownerID).catch(err => {});
+            if (!owner) return;
+            if (message.attachments.size != 0) {
+                let attachments = "";
+                message.attachments.forEach(attachment => {
+                    attachments += attachment.url + "\n"
+                });
+                owner.send(dmEmbed).catch(err => {});
+                return owner.send("With attachments: " + attachments)
+            }
+            return owner.send(dmEmbed).catch(err => {});
+        }
+    };
+    if (!message.channel.permissionsFor(client.user).has("SEND_MESSAGES")) return;
     let prefix;
     prefix = await client.db.r.table("guilds").get(message.guild.id).getField("prefix").run().catch(async err => {
         await client.db.createGuild(message.guild);
